fc3e1c5bd8658a7e7e5615dfa8f4f48f0697d610
dano2906 <dlg9806@gmail.com>
2024-03-26 14:54:47 -0400

    setup: Add tailwind, components and db

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
+import db from '@astrojs/db';
+import vue from '@astrojs/vue';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [
+        db(),
+        vue(),
+        tailwind({
+            applyBaseStyles: false,
+        })
+    ]
+});
